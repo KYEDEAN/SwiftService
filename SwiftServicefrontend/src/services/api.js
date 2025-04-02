d932eb0951f6ce8e2fa0ba3ce64a028014703787
RC <aarsheestar@gmail.com>
2025-04-02 17:06:32 +0545

    api.js changed

--- SwiftServicefrontend/src/services/api.js
+++ SwiftServicefrontend/src/services/api.js
@@ -21,3 +21,13 @@ export const fetchServiceDetail = async (id) => {
     return null;
   }
 };
+
+export const getProviders = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/providers/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching providers:', error);
+    return [];
+  }
+}
